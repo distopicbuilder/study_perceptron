30e05d564660c79d5a483c98b3199c319c880604
distopicbuilder <ramonnazaro.contato@gmail.com>
2022-08-14 09:21:37 -0300

    Change function for class

--- js/neural/perceptron.js
+++ js/neural/perceptron.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 export class Perceptron {
   weight = [];
   constructor(weightSize) {
@@ -20,3 +21,26 @@ export class Perceptron {
     return n >= 0 ? 1 : -1;
   }
 }
+=======
+// Activation function
+var sign = (n) => {
+  return n >= 0 ? 1 : -1;
+};
+export const perceptron = {
+  sizeOfWeight: 2,
+  weight: [],
+  initialization: () => {
+    for (let i = 0; i < perceptron.sizeOfWeight; i++) {
+      perceptron.weight[i] = random(-1, 1);
+    }
+  },
+  guess: (inputs) => {
+    let sum = 0;
+    for (let i = 0; i < perceptron.weight.length; i++) {
+      sum += inputs[i] * perceptron.weight.length[i];
+    }
+    let output = sign(sum);
+    return output;
+  },
+};
+>>>>>>> parent of 4282bac (Perceptron for class)
